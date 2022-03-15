c9f708270582a8022e0dfc43d81e3036ca492af3
Davyd McColl <davydm@gmail.com>
2022-03-15 15:49:32 +0200

    :recycle: move things about a bit to make it slightly easier to look at

---
+++ src/diff-buddy/Review.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using PeanutButter.EasyArgs;
+using PeanutButter.Utils;
+
+namespace diff_buddy;
+
+public static class Review
+{
+    public static int RunWith(Options options)
+    {
+        options.Review = false;
+        if (options.Repo == ".")
+        {
+            options.Repo = Environment.CurrentDirectory;
+        }
+
+        var cmd = Environment.CommandLine;
+        var splitCmd = new Queue<string>(cmd.Split(' '));
+        var exe = splitCmd.Dequeue().Replace(".dll", ".exe");
+        while (splitCmd.Count > 0 && exe.Count(c => c == '"') % 2 != 0)
+        {
+            exe = $"{exe} {splitCmd.Dequeue()}";
+        }
+
+        exe = exe.Trim('"');
+
+        options.CountOnly = true;
+        var pages = -1;
+        using (var io = ProcessIO.Start(exe, options.GenerateArgs().AsQuotedArgs()))
+        {
+            var result = io.StandardOutput.FirstOrDefault();
+            if (result is null || !int.TryParse(result, out pages))
+            {
+                Console.WriteLine($"Unable to count changes in {options.Repo}");
+                return 1;
+            }
+        }
+
+        options.CountOnly = false;
+
+        for (var i = 1; i < pages; i++)
+        {
+            Console.Clear();
+            options.At = i;
+            
+            RunOnce.With(options, true);
+
+            Console.Write($"[{i}/{pages}] Press any key to continue, q to quit...");
+            var c = Console.ReadKey();
+            if (c.KeyChar == 'q')
+            {
+                return 0;
+            }
+        }
+
+        return 0;
+    }
+}
---
+++ src/diff-buddy/RunOnce.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using LibGit2Sharp;
+using PeanutButter.Utils;
+using static diff_buddy.Functions;
+
+namespace diff_buddy;
+
+public static class RunOnce
+{
+    public static int With(Options options, bool showFilePathAtEnd)
+    {
+        if (options.At != 0)
+        {
+            options.Offset = options.At - 1;
+            options.Limit = 1;
+            options.ShowPatches = true;
+        }
+
+        using var repo = new Repository(options.Repo);
+        if (string.IsNullOrWhiteSpace(options.ToBranch))
+        {
+            options.ToBranch = repo.Head.FriendlyName;
+        }
+
+        var leftTree = repo.Branches.FirstOrDefault(b => b.FriendlyName == options.FromBranch)?.Tip.Tree;
+        var rightTree = repo.Branches.FirstOrDefault(b => b.FriendlyName == options.ToBranch)?.Tip.Tree;
+
+        var ignoreLineExpressions = (options.IgnoreLines ?? new string[0]).Select(s => new Regex(s)).ToArray();
+        var ignoreFileExpressions = (options.IgnoreFiles ?? new string[0]).Select(s => new Regex(s)).ToArray();
+
+        var toSkip = options.Offset;
+        var seen = 0;
+        var allChanges = repo.Diff.Compare<Patch>(leftTree, rightTree, new CompareOptions());
+        var indexSize = $"{Math.Min(options.Limit, allChanges.Count())}".Length;
+        foreach (var change in allChanges)
+        {
+            if (ignoreFileExpressions.Any(re => re.IsMatch(change.Path)))
+            {
+                continue;
+            }
+
+            var patchLines = FindCodeLines(change);
+            var added = FindInterestingLines(patchLines, "+", ignoreLineExpressions);
+            var removed = FindInterestingLines(patchLines, "-", ignoreLineExpressions);
+            if (added.IsEmpty() && removed.IsEmpty())
+            {
+                continue;
+            }
+
+            if (options.IgnoreWhitespace &&
+                AreSameWithoutWhitespace(added, removed))
+            {
+                continue;
+            }
+
+            if (--toSkip >= 0)
+            {
+                continue;
+            }
+
+            seen++;
+            if (showFilePathAtEnd)
+            {
+                ShowPatchIfRequired(options, patchLines);
+                PrintEntry(options, seen, indexSize, change);
+            }
+            else
+            {
+                PrintEntry(options, seen, indexSize, change);
+                ShowPatchIfRequired(options, patchLines);
+            }
+
+
+            if (seen >= options.Limit)
+            {
+                if (options.At == 0 && !options.CountOnly)
+                {
+                    Console.WriteLine($"Output limit reached: {options.Limit}");
+                }
+
+                break;
+            }
+        }
+
+        if (options.CountOnly)
+        {
+            Console.WriteLine($"{seen}");
+        }
+        else if (seen < options.Limit && !options.ShowIndexes)
+        {
+            Console.WriteLine($"{seen} results");
+        }
+
+        return 0;
+    }
+}
