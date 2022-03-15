df05b59addf90959fcb1dfc025ad9cccec54e7c5
Davyd McColl <davydm@gmail.com>
2022-03-15 17:19:25 +0200

    :bug: should observe paging options during review

--- src/diff-buddy/Review.cs
+++ src/diff-buddy/Review.cs
@@ -12,6 +12,10 @@ public static class Review
     public static int RunWith(Options options)
     {
         options.Review = false;
+        var originalOffset = options.Offset;
+        var originalLimit = options.Limit;
+        options.Offset = 0;
+        options.Limit = int.MaxValue;
         if (options.Repo == ".")
         {
             options.Repo = Environment.CurrentDirectory;
@@ -28,11 +32,11 @@ public static class Review
         exe = exe.Trim('"');
 
         options.CountOnly = true;
-        var pages = -1;
+        var entries = -1;
         using (var io = ProcessIO.Start(exe, options.GenerateArgs().AsQuotedArgs()))
         {
             var result = io.StandardOutput.FirstOrDefault();
-            if (result is null || !int.TryParse(result, out pages))
+            if (result is null || !int.TryParse(result, out entries))
             {
                 Console.WriteLine($"Unable to count changes in {options.Repo}");
                 return 1;
@@ -41,14 +45,16 @@ public static class Review
 
         options.CountOnly = false;
 
-        for (var i = 1; i < pages; i++)
+        var start = originalOffset + 1;
+        var end = Math.Min(entries, originalLimit + start);
+        for (var i = start; i < end; i++)
         {
             Console.Clear();
             options.At = i;
             
             RunOnce.With(options, true);
 
-            Console.Write($"[{i}/{pages}] Press any key to continue, q to quit...");
+            Console.Write($"[{i}/{entries}] Press any key to continue, q to quit...");
             var c = Console.ReadKey();
             if (c.KeyChar == 'q')
             {
